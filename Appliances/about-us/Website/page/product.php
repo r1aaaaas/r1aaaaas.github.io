b1423a0034600eb2820eea1adc4404da93db7c77
r1aaaaas <55227073+r1aaaaas@users.noreply.github.com>
2019-09-12 15:34:50 -0700

    first

---
+++ Appliances/about-us/Website/page/product.php
@@ -0,0 +1,321 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- min-height: 55px;
+    padding: 0 15px 15px; 
+
+    .navbar-brand{
+    float: left;
+    
+    min-height: 55px;
+    padding: 0 15px 15px;
+    
+  }-->
+<head>
+
+  <title>About Us</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="apple-touch-icon" sizes="76x76" href="../../Website/img/icon.png">
+  <link rel="icon" type="icon/png" href="../../Website/img/icon.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+
+  h1.header1{
+      color: #660000;
+  }
+  h1.header2{
+    border: solid black;
+    background-color: white;
+    color: black;
+  }
+  body{
+
+    font-family: Verdana, Geneva, sans-serif;
+  }
+  .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+      background-color: black;
+      color: #FFF;
+      padding: 1% 0%;
+      font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+      font-size: 1.3em;
+      border: 0;
+  }
+  .left{
+
+    float: left;
+  }
+   
+ 
+  .navbar-inverse .navbar-nav .active a, .navbar-inverse .navbar-nav .active a:focus, .navbar-inverse .navbar-nav {
+    color: #FFF;
+    background-color: black;
+    
+
+
+
+  }
+
+  
+
+  .navbar-inverse .navbar-nav li a{
+
+    color: #D5D5D5;
+  }
+  .carousel-caption{
+    top: 50%;
+    transform: translateY(-50%);
+    text-transform: uppercase;
+  }
+  .btn{
+    font-size: 18px;
+    color: #D3D3D3;
+    padding: 12px 22px;
+    background-color: orange;
+    border: 2px solid #FFF; 
+  }
+  .container{
+    margin: 4% auto;
+  }
+  #icon{
+    max-width: 200px;
+    margin: 1% auto;
+  }
+  footer{
+    width: 100%;
+    background-color: black;
+    padding: 5%;
+    color: #FFF;
+  }
+  .fa{
+    padding: 15px;
+    font-size: 25px;
+    color: #FFF;
+  }
+  .fa:hover{
+    color: #D5D5D5;
+    text-decoration: none;
+
+  }
+  .fa-facebook{
+    color: #365899;
+  }
+  .fa-twitter{
+
+    color: #1da1f2
+  }
+  .color{
+      color: #0000cc;
+      font-size: 5rem;
+  }
+  div.line{
+    background-color: #0000cc;
+  }
+ 
+
+
+  
+
+  @media (max-width: 900px){
+   .fa{
+      font-size: 20px;
+      padding: 10px;
+   }
+  }
+
+
+  @media (max-width: 600px){
+    .carousel-caption{
+      display: none;
+    }
+    #icon{
+      max-width: 150px;
+    }
+    h2{
+      font-size: 1.5em;
+    }
+
+  }
+
+
+  </style>
+</head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        
+      </div>
+
+      <div class="collapse navbar-collapse" id="MyNavbar">
+        
+        
+        
+        <a class="left" href="home.php"></a>
+          <ul class="nav navbar-nav navbar-left">
+            <li class="active"><a href="home.php">Home</a></li>
+            <li class="active"><a href="management.php">Management</a></li>
+            <li class="active"><a href="product.php">Products</a></li>
+            <li class="active"><a href="../../../appliances-shop/index.php">Buy Here!</a></li>
+            
+          </ul>   
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a>Contact Us</a></li>
+            <li class="active"><a href="https://www.facebook.com/malupetako08" class="fa fa-facebook"></a></li>
+            <li class="active"><a href="#" class="fa fa-twitter"></a></li>
+            <li class="active"><a href="#" class="fa fa-google"></a></li>
+          </ul>   
+      </div>
+    </div>
+  </nav>
+
+  <!--end slider -->
+  <div class="container text-center">
+    <div class="row">
+      <div>
+      <h1 class="header2">Product</h1>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container text-center">
+    <div class="row">
+      <h1>Air Conditioner</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/ac1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>An air conditioner is a system or a machine that treats air in a defined, usually enclosed area via a refrigeration cycle in which warm air is removed and replaced with cooler air.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Electric Fan</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/fan1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>Mechanical devices that move air and other gases are essential to human comfort and safety and to some industrial processes. Broadly speaking they are of three types. Fans deliver large amounts of air or gas at low pressure, blowers have high rates of delivery at medium pressure, and compressors deliver at high pressure.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Microwave Oven</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/oven1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>Microwave Oven is an electric oven that heats and cooks food by exposing it to electromagnetic radiation in the microwave frequency range. Microwave ovens heat foods quickly and efficiently because excitation is fairly uniform in the outer 25–38 mm (1–1.5 inches) of a homogeneous, high water content food item.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Radio</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/rad1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>Radio is a hardware device that is capable of receiving and playing streamed media from either Internet radio stations or a home network.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Refrigerator</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/ref1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>A refrigerator consists of a thermally insulated compartment and a heat pump (mechanical, electronic or chemical) that transfers heat from the inside of the fridge to its external environment so that the inside of the fridge is cooled to a temperature below the ambient temperature of the room. Refrigeration is an essential food storage technique in developed countries. </h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Rice Cooker</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/rice1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>Rice Cooker is an automated kitchen appliance designed to boil or steam rice. It consists of a heat source, a cooking bowl, and a thermostat. The thermostat measures the temperature of the cooking bowl and controls the heat. Complex rice cookers may have many more sensors and other components, and may be multipurpose.</h3>
+      </div>
+    </div>
+
+     <div class="row">
+      <h1>Stove</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/st1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>Stove is a kitchen appliance designed for the purpose of cooking food. Kitchen stoves rely on the application of direct heat for the cooking process and may also contain an oven, used for baking. "Cookstoves" (also called "cooking stoves" or "wood stoves") are heated by burning wood or charcoal; "gas stoves" are heated by gas; and "electric stoves" by electricity.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Television</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/tv1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>Television is a telecommunication medium used for transmitting moving images in monochrome (black and white), or in colour, and in two or three dimensions and sound. The term can refer to a television set, a television program ("TV show"), or the medium of television transmission. Television is a mass medium for advertising, entertainment and news.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Vacuum Cleaner</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/vac1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>Vacuum Cleaner is a device that causes suction in order to suck up dirt/particles and dust from floors, upholstery, draperies and other surfaces. It is generally electrically driven.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <h1>Washing Machine</h1>
+      <div class="col-md-2">
+        <img src="../../Website/img/wm1.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-10">
+        <h3>A washing machine (laundry machine, clothes washer, or washer) is a home appliance used to wash laundry. The term is mostly applied to machines that use water as opposed to dry cleaning (which uses alternative cleaning fluids, and is performed by specialist businesses) or ultrasonic cleaners. The user adds laundry detergent, which is sold in liquid or powder form, to the wash water.</h3>
+      </div>
+    </div>
+  </div>
+
+  <footer class="container-fluid text-center">
+    <div class="row">
+      <div class="col-sm-6">
+        <h3> Developed By </h3>
+        <br>
+        <h4>Jerome M. Antiado</h4>
+        <h4>And</h4>
+        <h4>John Glenn B. Sabalbarino</h4>
+      </div>
+      <!--<div class="col-sm-">
+        <h3>Connect</h3>
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-google"></a>
+        <a href="#" class="fa fa-linkedin"></a>
+        <a href="#" class="fa fa-youtube"></a>
+      </div>-->
+      <div class="col-sm-6">
+        <img src="../../Website/img/qwert1.jpg" width="200" height="200">
+      </div>
+
+    </div>
+  </footer>
+
+
+</body>
+</html>
